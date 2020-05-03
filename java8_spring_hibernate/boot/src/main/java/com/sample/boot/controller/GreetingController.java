a818f2f21e3b0908bb23ebaa9a049bba538e75ff
linh <linhphan1987@gmail.com>
2020-05-03 10:49:22 +0700

    create boot project & add first configuration

---
+++ java8_spring_hibernate/boot/src/main/java/com/sample/boot/controller/GreetingController.java
@@ -0,0 +1,48 @@
+package com.sample.boot.controller;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sample.boot.dto.Greeting;
+
+/**
+ * boot/greeting
+ */
+@RestController()
+@RequestMapping("/greeting")
+public class GreetingController {
+
+	@Value("${test.property}")
+	private String applicationValue;
+	
+	@Value("${array.numbers}")
+	private List<Integer> arrayNumber;
+	
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	/**
+	 * boot/greeting 
+	 */
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	/**
+	 * boot/greeting/app-config-value
+	 * test get configuration value from application.properties
+	 */
+	@GetMapping("/app-config-value")
+	public String appConfigValue() {
+		return applicationValue + " " + arrayNumber;
+	}
+}
